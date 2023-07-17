b19c10b59fb816256934bf7dbe4081ae5f7326f8
dVera17 <dsvera174@gmail.com>
2023-07-17 17:26:05 -0500

    feat: :sparkles: definicion del endpoint para obtener los usuarios ordenados alfabeticmente por nombre

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 import express from 'express';
+import routesPaciente from './routes/pacientes.routes.js';
 const app = express();
 app.set('port', 5010);
+
+app.use('/pacientes_ordenados', routesPaciente);
 
 export default app;
