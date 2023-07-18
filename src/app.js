4eb93a144b0489c7c893a772f0e2618eeed369f2
dVera17 <dsvera174@gmail.com>
2023-07-18 16:01:44 -0500

    feat: :sparkles: Se define el endpoint para obtener todos los medicos con una especialidad especifica
    Se define el endpoint para obtener todos los medicos con una especialidad especifica

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import routesPaciente from './routes/pacientes.routes.js';
 import routesCitas from './routes/citas.routes.js';
+import routesDoctors from './routes/medicos.routes.js';
 const app = express();
 app.set('port', 5010);
 
 app.use('/pacientes_ordenados', routesPaciente);
 app.use('/citas_fecha', routesCitas);
+app.use('/medicos', routesDoctors);
 
 export default app;
