f0f8fbdcb7875b0f93069093f98062f9ac96c54b
dVera17 <dsvera174@gmail.com>
2023-07-18 15:27:48 -0500

    feat: :sparkles: Se define el endpoint para obtener las citas por orden de fecha
    Se define el endpoint para obtener las citas por orden de fecha mas cercanas

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import routesPaciente from './routes/pacientes.routes.js';
+import routesCitas from './routes/citas.routes.js';
 const app = express();
 app.set('port', 5010);
 
 app.use('/pacientes_ordenados', routesPaciente);
+app.use('/citas_fecha', routesCitas);
 
 export default app;
